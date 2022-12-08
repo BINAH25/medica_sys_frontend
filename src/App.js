e0284fed3af1e0fe40bdbe340a92a6713814ae44
BINAH LOUIS <visteen192@gmail.com>
2022-12-08 10:10:36 +0000

    company pages and components added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Main from "./components/Main";
+import Company from "./components/company/Company";
 function App() {
   return (
     <Router>
       <Routes>
         <Route element={<Login />} path="" />
         <Route element={<Main />} path="/home" />
+        <Route element={<Company />} path="/company" />
       </Routes>
     </Router>
   );
---
+++ src/pages/CompanyPages.js
@@ -0,0 +1,106 @@
+import React from "react";
+
+const CompanyPages = () => {
+  return (
+    <section className="content">
+      <div className="container-fluid">
+        <div className="block-header">
+          <h2>MANAGE COMPANY</h2>
+        </div>
+        <div className="row clearfix">
+          <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div className="card">
+              <div className="header">
+                <h2>Add Company</h2>
+              </div>
+              <div className="body">
+                <form>
+                  <label htmlFor="email_address">Name</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        className="form-control"
+                        placeholder="Enter Company Name"
+                      />
+                    </div>
+                  </div>
+                  <label htmlFor="email_address">License No.</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="text"
+                        id="license_no"
+                        name="license_no"
+                        className="form-control"
+                        placeholder="Enter License No."
+                      />
+                    </div>
+                  </div>
+                  <label htmlFor="email_address">Address</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="text"
+                        id="address"
+                        name="address"
+                        className="form-control"
+                        placeholder="Enter Company Address"
+                      />
+                    </div>
+                  </div>
+                  <label htmlFor="email_address">Contact No.</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="text"
+                        id="contact_no"
+                        name="contact_no"
+                        className="form-control"
+                        placeholder="Enter Contact No."
+                      />
+                    </div>
+                  </div>
+                  <label htmlFor="email_address">Email</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        className="form-control"
+                        placeholder="Enter Company Email"
+                      />
+                    </div>
+                  </div>
+                  <label htmlFor="email_address">Description</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="text"
+                        id="description"
+                        name="description"
+                        className="form-control"
+                        placeholder="Enter Description"
+                      />
+                    </div>
+                  </div>
+                  <br />
+                  <button
+                    type="submit"
+                    className="btn btn-primary m-t-15 waves-effect btn-block"
+                  ></button>
+                  <br />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CompanyPages;
