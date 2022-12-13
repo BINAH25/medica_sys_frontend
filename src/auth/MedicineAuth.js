c4af5ab2e688c3ce2983ef2b1e934a1b51211ab6
BINAH LOUIS <visteen192@gmail.com>
2022-12-13 08:56:13 +0000

    medicine auth added

---
+++ src/auth/MedicineAuth.js
@@ -0,0 +1,111 @@
+import axios from "axios";
+import Config from "./Config";
+import Auth from "./Auth";
+class MedicineAuth {
+  static createMedicine = (
+    name,
+    medical_typ,
+    buy_price,
+    sell_price,
+    c_gst,
+    s_gst,
+    batch_no,
+    shelf_no,
+    expire_date,
+    mfg_date,
+    company_id,
+    description,
+    in_stock_total,
+    qty_in_strip,
+    callback
+  ) => {
+    axios
+      .post(
+        Config.medicineUrl,
+        {
+          name: name,
+          medical_typ: medical_typ,
+          buy_price: buy_price,
+          sell_price: sell_price,
+          c_gst: c_gst,
+          s_gst: s_gst,
+          batch_no: batch_no,
+          shelf_no: shelf_no,
+          expire_date: expire_date,
+          mfg_date: mfg_date,
+          company_id: company_id,
+          description: description,
+          in_stock_total: in_stock_total,
+          qty_in_strip: qty_in_strip,
+        },
+        { headers: { Authorization: "Bearer " + Auth.getLoginToken() } }
+      )
+      .then(function (response) {
+        if (response.status === 200) {
+          callback({ error: "false", message: "Medicine added Successful..." });
+        }
+      })
+      .catch(function (error) {
+        callback({
+          error: "true",
+          message: "Error, Failed to Add Medicine..",
+        });
+      });
+  };
+
+  static editMedicine = (
+    name,
+    medical_typ,
+    buy_price,
+    sell_price,
+    c_gst,
+    s_gst,
+    batch_no,
+    shelf_no,
+    expire_date,
+    mfg_date,
+    company_id,
+    description,
+    in_stock_total,
+    qty_in_strip,
+    callback
+  ) => {
+    axios
+      .put(
+        Config.medicineUrl + id + "/",
+        {
+          name: name,
+          medical_typ: medical_typ,
+          buy_price: buy_price,
+          sell_price: sell_price,
+          c_gst: c_gst,
+          s_gst: s_gst,
+          batch_no: batch_no,
+          shelf_no: shelf_no,
+          expire_date: expire_date,
+          mfg_date: mfg_date,
+          company_id: company_id,
+          description: description,
+          in_stock_total: in_stock_total,
+          qty_in_strip: qty_in_strip,
+        },
+        { headers: { Authorization: "Bearer " + Auth.getLoginToken() } }
+      )
+      .then(function (response) {
+        if (response.status === 200) {
+          callback({
+            error: "false",
+            message: "Medicine Updated Successful...",
+          });
+        }
+      })
+      .catch(function (error) {
+        callback({
+          error: "true",
+          message: "Error, Failed to Update Medicine..",
+        });
+      });
+  };
+}
+
+export default MedicineAuth;
