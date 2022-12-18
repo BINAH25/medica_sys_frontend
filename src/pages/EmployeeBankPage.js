8c5b6d37c96685cc1ef66547709f60e502ec0496
BINAH LOUIS <visteen192@gmail.com>
2022-12-18 12:00:00 +0000

    add employee bank implemented successfuly

--- src/pages/EmployeeBankPage.js
+++ src/pages/EmployeeBankPage.js
@@ -102,7 +102,144 @@ const EmployeeBankPage = () => {
     setEmployees(res.data);
   };
 
-  return <div>EmployeeBankPage</div>;
+  return (
+    <section className="content">
+      <div className="container-fluid">
+        <div className="col-xs-12">{getMessage()}</div>
+        <div className="block-header">
+          <h2>MANAGE EMPLOYEE BANK</h2>
+        </div>
+        <div className="row clearfix">
+          <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div className="card">
+              <div className="header">
+                <h2>Add Employee Bank</h2>
+              </div>
+              <div className="body">
+                <form method="post" onSubmit={handleSubmit}>
+                  <label htmlFor="email_address">BANK ACCOUNT NO</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="text"
+                        id="bank_account_no"
+                        name="bank_account_no"
+                        className="form-control"
+                        placeholder="Enter Bank Account No"
+                        value={employeeBank.bank_account_no}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                  </div>
+                  <label htmlFor="email_address">IFSC No.</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="text"
+                        id="ifsc_no"
+                        name="ifsc_no"
+                        className="form-control"
+                        placeholder="Enter License No."
+                        value={employeeBank.ifsc_no}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                  </div>
+                  <label htmlFor="email_address">Employee</label>
+                  <div className="form-group">
+                    <select
+                      className="form-control show-tick"
+                      name="employee_id"
+                      id="employee_id"
+                      onChange={handleChange}
+                      value={employeeBank.employee_id}
+                    >
+                      {employees.map((employee) => (
+                        <option key={employee.id} value={employee.id}>
+                          {employee.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>{" "}
+                  <br />
+                  <button
+                    type="submit"
+                    className="btn btn-primary m-t-15 waves-effect btn-block"
+                  >
+                    Add Employee Bank
+                  </button>
+                  <br />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row clearfix">
+          <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div className="card">
+              <div className="header">
+                {employeeBank.dataLoaded == false ? (
+                  <div className="text-center">
+                    <div className="preloader pl-size-xl">
+                      <div className="spinner-layer">
+                        <div className="circle-clipper left">
+                          <div className="circle"></div>
+                        </div>
+                        <div className="circle-clipper right">
+                          <div className="circle"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  ""
+                )}
+                <h2>All Employee Banks</h2>
+              </div>
+              <div className="body table-responsive">
+                <table className="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>#ID</th>
+                      <th>BANK ACCOUNT NO</th>
+                      <th>IFSC NO.</th>
+                      <th>EMPLOYEE ID</th>
+                      <th>EMPLOYEE NAME</th>
+                      <th>ADDED ON</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {getEmployeeBank.map((employee_bank, index) => (
+                      <tr key={index}>
+                        <td>{employee_bank.id}</td>
+                        <td>{employee_bank.bank_account_no}</td>
+                        <td>{employee_bank.ifsc_no}</td>
+                        <td>{employee_bank.employee_id}</td>
+                        <td>{employee_bank.employee?.name}</td>
+                        <td>
+                          {new Date(employee_bank.added_on).toLocaleString()}
+                        </td>
+                        <td>
+                          <Link
+                            className="btn btn-block btn-primary"
+                            to={`/employee_bank/${employee_bank.id}`}
+                          >
+                            View
+                          </Link>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default EmployeeBankPage;
