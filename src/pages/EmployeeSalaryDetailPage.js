cd3b4af63628443bd922938e3dcf9449d2e8e6de
BINAH LOUIS <visteen192@gmail.com>
2022-12-19 12:20:57 +0000

    return function added in EmployeeSalaryDetailPage

--- src/pages/EmployeeSalaryDetailPage.js
+++ src/pages/EmployeeSalaryDetailPage.js
@@ -112,7 +112,142 @@ const EmployeeSalaryDetailPage = () => {
     setEmployees(res.data);
   };
 
-  return <div>EmployeeSalaryDetailPage</div>;
+  return (
+    <section className="content">
+      <div className="container-fluid">
+        <div className="col-xs-12">{getMessage()}</div>
+        <div className="block-header">
+          <h2>MANAGE EMPLOYEE SALARY</h2>
+        </div>
+        <div className="row clearfix">
+          <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div className="card">
+              <div className="header">
+                <h2>Add Employee Salary</h2>
+              </div>
+              <div className="body">
+                <form method="post" onSubmit={handleSubmit}>
+                  <label htmlFor="email_address">Employee</label>
+                  <div className="form-group">
+                    <select
+                      className="form-control show-tick"
+                      name="employee_id"
+                      id="employee_id"
+                      onChange={handleChange}
+                      value={employeeSalary.employee_id}
+                    >
+                      {employees.map((employee) => (
+                        <option key={employee.id} value={employee.id}>
+                          {employee.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <label htmlFor="email_address">SALARY DATE</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="date"
+                        id="salary_date"
+                        name="salary_date"
+                        className="form-control"
+                        placeholder="Enter salary date"
+                        value={employeeSalary.salary_date}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                  </div>
+                  <label htmlFor="email_address">SALARY AMOUNT</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="text"
+                        id="salary_amount"
+                        name="salary_amount"
+                        className="form-control"
+                        placeholder="Enter salary_amount ."
+                        value={employeeSalary.salary_amount}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                  </div>
+                  <br />
+                  <button
+                    type="submit"
+                    className="btn btn-primary m-t-15 waves-effect btn-block"
+                  >
+                    Add Employee Salary
+                  </button>
+                  <br />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row clearfix">
+          <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div className="card">
+              <div className="header">
+                {employeeSalary.dataLoaded == false ? (
+                  <div className="text-center">
+                    <div className="preloader pl-size-xl">
+                      <div className="spinner-layer">
+                        <div className="circle-clipper left">
+                          <div className="circle"></div>
+                        </div>
+                        <div className="circle-clipper right">
+                          <div className="circle"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  ""
+                )}
+                <h2>All Employee Salaries </h2>
+              </div>
+              <div className="body table-responsive">
+                <table className="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>#ID</th>
+                      <th>SALARY DATE</th>
+                      <th>SALARY AMOUNT</th>
+                      <th>EMPLOYEE ID</th>
+                      <th>EMPLOYEE NAME</th>
+                      <th>ADDED ON</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {getEmployeeSalaries.map((salary, index) => (
+                      <tr key={index}>
+                        <td>{salary.id}</td>
+                        <td>{salary.salary_amount}</td>
+                        <td>{salary.salary_date}</td>
+                        <td>{salary.employee_id}</td>
+                        <td>{salary.employee?.name}</td>
+                        <td>{new Date(salary.added_on).toLocaleString()}</td>
+                        <td>
+                          <Link
+                            className="btn btn-block btn-primary"
+                            to={`/employee_salary/${salary.id}`}
+                          >
+                            View
+                          </Link>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default EmployeeSalaryDetailPage;
