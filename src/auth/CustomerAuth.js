96885a5216d4dbc68e7ee3c85232c8e40c3a4ccd
BINAH LOUIS <visteen192@gmail.com>
2022-12-20 07:20:55 +0000

    customer auth added

---
+++ src/auth/CustomerAuth.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+import Config from "./Config";
+import Auth from "./Auth";
+class CustomerAuth {
+  static createCustomer = (name, address, contact, callback) => {
+    axios
+      .post(
+        Config.companyUrl,
+        {
+          name: name,
+          address: address,
+          contact: contact,
+        },
+        { headers: { Authorization: "Bearer " + Auth.getLoginToken() } }
+      )
+      .then(function (response) {
+        if (response.status === 200) {
+          callback({ error: "false", message: "Customer added Successful..." });
+        }
+      })
+      .catch(function (error) {
+        callback({
+          error: "true",
+          message: "Error, Failed to Add Customer..",
+        });
+      });
+  };
+
+  static editCompany = (name, address, contact, id, callback) => {
+    axios
+      .put(
+        Config.companyUrl + id + "/",
+        {
+          name: name,
+          address: address,
+          contact: contact,
+        },
+        { headers: { Authorization: "Bearer " + Auth.getLoginToken() } }
+      )
+      .then(function (response) {
+        if (response.status === 200) {
+          callback({
+            error: "false",
+            message: "Customer Updated Successful...",
+          });
+        }
+      })
+      .catch(function (error) {
+        callback({
+          error: "true",
+          message: "Error, Failed to Update Customer..",
+        });
+      });
+  };
+
+  // url to get all company
+}
+
+export default CustomerAuth;
