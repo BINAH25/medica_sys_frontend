804fc860f530be1f44ec7e49da32bcc5447dbec5
BINAH LOUIS <visteen192@gmail.com>
2022-12-21 09:31:31 +0000

    bill detail auth added

---
+++ src/auth/BillDetailAuth.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import Config from "./Config";
+import Auth from "./Auth";
+class BillDetailAuth {
+  static createBill = (bill_id, medicine_id, qty, callback) => {
+    axios
+      .post(
+        Config.billUrl,
+        {
+          bill_id: bill_id,
+          medicine_id: medicine_id,
+          qty: qty,
+        },
+        { headers: { Authorization: "Bearer " + Auth.getLoginToken() } }
+      )
+      .then(function (response) {
+        if (response.status === 200) {
+          callback({
+            error: "false",
+            message: "BillDetail added Successful...",
+          });
+        }
+      })
+      .catch(function (error) {
+        callback({
+          error: "true",
+          message: "Error, Failed to Add BillDetail..",
+        });
+      });
+  };
+
+  // url to get all company
+}
+
+export default BillDetailAuth;
