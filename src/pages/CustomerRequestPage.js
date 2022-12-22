7756d702f3621a8d6397dc420f8499843077512a
BINAH LOUIS <visteen192@gmail.com>
2022-12-22 09:42:17 +0000

    everything working successfully

--- src/pages/CustomerRequestPage.js
+++ src/pages/CustomerRequestPage.js
@@ -1,4 +1,3 @@
-import React from "react";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Auth from "../auth/Auth";
@@ -7,7 +6,7 @@ import { Link } from "react-router-dom";
 import CustomerRequestAuth from "../auth/CustomerRequestAuth";
 // MAIN FUNCTION
 const CustomerRequestPage = () => {
-  const [getRequets, setGetRequets] = useState([]);
+  const [getRequests, setGetRequests] = useState([]);
   const [customerRequest, setCustomerRequest] = useState({
     customer_name: "",
     phone: "",
@@ -85,14 +84,149 @@ const CustomerRequestPage = () => {
     getAllCustomerRequest();
   }, []);
   let getAllCustomerRequest = async () => {
-    let res = await axios.get(Config.companyBankUrl, {
+    let res = await axios.get(Config.customerRequestUrl, {
       headers: { Authorization: "Bearer " + Auth.getLoginToken() },
     });
-    setGetRequets(res.data);
+    setGetRequests(res.data);
     setCustomerRequest({ dataLoaded: true });
   };
 
-  return <div>CustomerRequestPage</div>;
+  return (
+    <section className="content">
+      <div className="container-fluid">
+        <div className="col-xs-12">{getMessage()}</div>
+        <div className="block-header">
+          <h2>MANAGE CUSTOMER REQUEST</h2>
+        </div>
+        <div className="row clearfix">
+          <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div className="card">
+              <div className="header">
+                <h2>Add customer request</h2>
+              </div>
+              <div className="body">
+                <form method="post" onSubmit={handleSubmit}>
+                  <label htmlFor="email_address">Customer Name</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="text"
+                        id="customer_name"
+                        name="customer_name"
+                        className="form-control"
+                        placeholder="Enter Customer Name"
+                        value={customerRequest.customer_name}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                  </div>
+                  <label htmlFor="email_address">Phone</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="text"
+                        id="phone"
+                        name="phone"
+                        className="form-control"
+                        placeholder="Enter phone No."
+                        value={customerRequest.phone}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                  </div>
+                  <label htmlFor="email_address">Medicine Detail.</label>
+                  <div className="form-group">
+                    <div className="form-line">
+                      <input
+                        type="text"
+                        id="medicine_details"
+                        name="medicine_details"
+                        className="form-control"
+                        placeholder="Enter medicine details "
+                        value={customerRequest.medicine_details}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <br />
+                  <button
+                    type="submit"
+                    className="btn btn-primary m-t-15 waves-effect btn-block"
+                  >
+                    Add Customer Request
+                  </button>
+                  <br />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row clearfix">
+          <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div className="card">
+              <div className="header">
+                {customerRequest.dataLoaded == false ? (
+                  <div className="text-center">
+                    <div className="preloader pl-size-xl">
+                      <div className="spinner-layer">
+                        <div className="circle-clipper left">
+                          <div className="circle"></div>
+                        </div>
+                        <div className="circle-clipper right">
+                          <div className="circle"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  ""
+                )}
+                <h2>All Customer Requests</h2>
+              </div>
+              <div className="body table-responsive">
+                <table className="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>#ID</th>
+                      <th>CUSTOMER NAME</th>
+                      <th>PHONE</th>
+                      <th>MEDICINE DETAIL</th>
+                      <th>COMPANY NAME</th>
+                      <th>ADDED ON</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {getRequests.map((request, index) => (
+                      <tr key={index}>
+                        <td>{request.id}</td>
+                        <td>{request.customer_name}</td>
+                        <td>{request.phone}</td>
+                        <td>{request.medicine_details}</td>
+                        <td>{request.status}</td>
+                        <td>{new Date(request.added_on).toLocaleString()}</td>
+                        <td>
+                          <Link
+                            className="btn btn-block btn-primary"
+                            to={`/customer_request/${request.id}`}
+                          >
+                            View
+                          </Link>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default CustomerRequestPage;
